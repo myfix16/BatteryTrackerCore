52648c3d11c7bb0703b55555d947b38a9e35bd2a
myfix16 <myfix16@outlook.com>
2020-09-08 20:58:47 +0800

    Allow user to change scan interval.

---
+++ BatteryTracker/Win32API.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace BatteryTracker
+{
+/*    internal class Win32API
+    {
+        /// <summary>
+        /// The function retrieves system power status.
+        /// </summary>
+        /// <param name="sps"></param>
+        /// <returns>
+        ///     If the function succeeds, the return value is nonzero. Otherwise, the return value is zero.
+        /// </returns>
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool GetSystemPowerStatus(out SystemPowerStatus sps);
+
+        /// <summary>
+        /// Returns true if a an AC Power Line is detected
+        /// </summary>
+        public static bool ACPowerPluggedIn()
+        {
+            SystemPowerStatus SPS = new SystemPowerStatus();
+            GetSystemPowerStatus(out SPS);
+
+            return (SPS.LineStatus == ACLineStatus.Online);
+        }
+
+        /// <summary>
+        /// Returns an integer representing the life time of battery charge remaining.
+        /// </summary>
+        public static int GetBatteryLifeTime()
+        {
+            SystemPowerStatus SPS = new SystemPowerStatus();
+            GetSystemPowerStatus(out SPS);
+
+            return (int)SPS.BatteryLifeTime;
+        }
+    }
+
+    public enum ACLineStatus : byte
+    {
+        Offline = 0,
+        Online = 1,
+        Unknown = 255
+    }
+
+    public enum BatteryFlag : byte
+    {
+        High = 1,
+        Low = 2,
+        Critical = 4,
+        Charging = 8,
+        NoSystemBattery = 128,
+        Unknown = 255
+    }
+
+    public struct SystemPowerStatus
+    {
+        public ACLineStatus LineStatus;
+        public BatteryFlag flgBattery;
+        public byte BatteryLifePercent;
+        public byte Reserved1;
+        public int BatteryLifeTime;
+        public int BatteryFullLifeTime;
+    }*/
+}
---
+++ BatteryTrackerFramework/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BatteryTracker
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var trayIcon = new TrayIcon();
+            Application.Run();
+        }
+    }
+}
